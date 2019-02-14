cea1e2ca120873cc524da8010eba2f6c54a9afd2
Bartosz Miedlar <bmiedlar@wp.pl>
2019-02-14 17:39:43 +0100

    init commit

---
+++ src/main/java/com/bmiedlar/twitter/TwitterFeederApplication.java
@@ -0,0 +1,12 @@
+package com.bmiedlar.twitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterFeederApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TwitterFeederApplication.class, args);
+  }
+}
